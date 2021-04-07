82253ed6a83c322404ecffa0d3a5d91cabd1e3c5
AntNZ <>
2021-04-07 01:23:38 +0000

    Script downloads file containing user information from remote repo, copys the user csv file to the top level directory then deletes the cloned repo directory. Function started to parse a user name, to create a user.

--- Task-1/task1.sh
+++ Task-1/task1.sh
@@ -1,5 +1,37 @@
 #!/bin/bash
 
+# Global Variables
+
+downloadSuccess=false
+
+# Function Declarations
+
+# Check If Group Exists, 
+# If Not, Create Group
+# $1 = group
+checkGroup() {
+if grep -q $1 /etc/group
+    then
+        echo "$1 already exists"
+    else
+        echo "$1 does not exist"
+        # create group
+    fi
+}
+
+createUserName() {
+    name="new name"
+    echo $name: $1
+}
+
+addUserToGroup() {
+    echo $1
+}
+
+alternativeDownloadOnFail() {
+    echo $1
+}
+
 # Download User.csv from Github Repo URL
 git clone https://github.com/leggant/2021-OS-Assignment.git
 cd 2021-OS-Assignment/
@@ -7,9 +39,11 @@ git checkout automated-io
 git status
 
 # move file out of folder into top level dir
-mv Users.csv ../
+cp Users.csv ../
 cd ../
+rm -rf 2021-OS-Assignment
 pwd
+
 
 # Check If Users File Exists - Make A Function
 FILE=Users.csv
@@ -26,11 +60,7 @@ else
     echo "Please enter a new file url: "
 fi
 
-
 # Check if file is parsable. - Make A Function, change user execution persission if needed.
-
-
-
 
 # Parse User File
 
@@ -38,13 +68,21 @@ fi
     read
     while IFS=";", read -r email dob group shared
     do
+        # create username from email
+        # format first letter of first name followed by full last name.
+        echo "Converting $email to username"
+        createUserName $email
         password=$(date -d $dob +'%m%Y')
         echo "Converting $dob to Password: $password"
         echo "Converting $email to username:"
-        # first name
+        
 
-        # last name
         echo "Groups: $group"
+        #parse group string
+
+        #check if group exists
+        #create if not
+        #add this user to group
         echo "Shared Folder: $shared"
         echo
     done
@@ -54,3 +92,5 @@ fi
 
 # set -x will force the output of debugger
 set -x
+
+
