8acf535fb9efeb0bcf8be6715b5a0a8fe91f51d7
Anthony Legg <>
2021-04-11 23:21:35 +0000

    Update to tutorial file - non-assignment

--- Task-1/test.sh
+++ Task-1/test.sh
@@ -10,8 +10,36 @@ echo "Hi Yogi And Bella" >> test.txt
 Multiline Comments'
 
 cat << outputComments
-this is hello create text
-this is hello create text
-this is hello create text
-and another
+Conditional
+Statements
 outputComments
+
+count=4
+if [ $count -eq 9 ]
+then
+    echo "Count is equal to $count"
+elif (( $count <= 5 ))
+then
+    echo "the condition is less than or eq to five"
+else
+    echo "Count is not equal to $count"
+fi
+
+: '
+-ne = not equal || -gt = greater than 
+or (( $count > 9 )) replace [ ] wrapper with (( ))
+or (( $count < 9 ))
+'
+
+# And Operator
+
+age=110
+if [ $age -gt 18 ] && [ $age -lt 50 ]
+then
+    echo "Age is ok to enter"
+elif [ $age -eq 100 ] || [ $age -gt 100 ]
+then
+    echo "Wow You Are Old!"
+else
+    echo "Nope!"
+fi
