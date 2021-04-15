b86cd64eca68e8a735d096f0c3034607757d65e0
Anthony Legg <>
2021-04-15 23:24:45 +0000

    Updates to functions, still not working.

--- Task-1/task1.sh
+++ Task-1/task1.sh
@@ -1,23 +1,25 @@
 #!/bin/bash
-# Download Default User.csv from Github Repo URL
-downloadDefaultCSV() {
-    git clone https://github.com/leggant/2021-OS-Assignment.git
-    cd 2021-OS-Assignment/
-    git checkout automated-io
-    git status
-    cp Users.csv ../
-    cd ../
-    #rm -rf 2021-OS-Assignment
+
+default="http://kate.ict.op.ac.nz/~faisalh/IN617linux/users.csv"
+# Check Download URI resource, check it starts with http:// 
+# and ends in .csv
+checkCSV_URI() {
+    echo $1
 }
 
-downloadAlternateCSV() {
-    #wget $1
-    #git clone $1
+# Download Default user.csv
+downloadDefaultCSV() {
     echo $1
 }
 
 checkFileExists() {
     echo "checking"
+    FILE=$1
+    if [ -f "$FILE" ]; then
+        echo -e "$FILE exists.    " 
+    else 
+        echo "$FILE does not exist."
+    fi
 }
 
 checkFileIsParsable() {
@@ -25,11 +27,20 @@ checkFileIsParsable() {
 }
 
 checkIfGroupExists() {
-    echo $1
+    if [ grep -q $1 /etc/group]; then
+        echo "$1 already exists"
+    else
+        echo "$1 does not exist"
+        # create group
+    fi
 }
 
 createUser() {
-    echo $1
+    xname=$1
+    initial=${xname:0:1}
+    last=$(echo $xname | cut -d"@" -f1 | cut -d"." -f2)
+    name=$initial$last
+    echo "Converted $1 to username: $name"
 }
 
 createGroup () {
@@ -37,36 +48,27 @@ createGroup () {
 }
 
 echo -e "\nThis script will auto new user creation on this system. Do you wish to: \n
-1) download the default CSV file from GitHub 
-2) use the default locally stored CSV file?
-3) enter a path to a new local CSV file, or
-4) enter a different GitHub URL "
+1) Download and Use the Default CSV File 
+2) Enter a New URL to a CSV File For Download
+
+"
 
 x=1
-until [[ $x -eq 4 || $option -eq $x ]]
+until [[ $x -eq 4 || $option -eq 1 || $option -eq 2 ]]
 do
-    read -p "Enter 1, 2, 3 or 4: " option
+    read -p "Enter 1 or 2: " option
     case $option in 
         1*) 
-            echo -e "\t\nDownload Users From GitHub\n";
-            downloadDefaultCSV;
-            # check successful 
-            # then exit - if Error, 
-            # then..... exit 1
+            echo -e "\tChecking Users CSV File URL\n";
+            checkCSV_URI $default;
+            echo -e "\tDownloading Users CSV File\n";
+            downloadDefaultCSV $default;
             exit 0;;
         2*) 
             echo -e "\t\nChecking Default Local User File"; 
             exit 0 ;;
-        3*) 
-            echo -e "\t\nChecking New Local User File"; 
-            exit 0 ;;
-        4*) 
-            read -p "Enter the GitHub Repos URL: " repo;
-            echo -e "\t\nDownloading New GitHub Repo"; 
-            downloadAlternateCSV $repo;
-            exit 0 ;;
         *) 
-            echo -e "\f\t>>Error, Please try again\n" ;;
+            echo -e "\f\t>> Error, Please try again <<\n" ;;
     esac 
     x=$(( x+1 ))
     if [ $x -eq 4 ]; then
@@ -75,53 +77,25 @@ do
     fi
 done
 
-# Check If Group Exists, 
-# If Not, Create Group
-# $1 = group
-
-# if grep -q $1 /etc/group
-#     then
-#         echo "$1 already exists"
-#     else
-#         echo "$1 does not exist"
-#         # create group
-#     fi
-
-# xname=$1
-# initial=${xname:0:1}
-# last=$(echo $xname | cut -d"@" -f1 | cut -d"." -f2)
-# name=$initial$last
-# echo "Converted $1 to username: $name"
-
-# Check If Users File Exists - Make A Function
-# FILE=Users.csv
-# if [ -f "$FILE" ]; then
-#     echo -e "$FILE exists.
-#     " 
-# else 
-#     echo "$FILE does not exist."
-#     echo "Please enter a new file url: "
-# fi
-
 # # Check if file is parsable. - Make A Function, 
 # # change user execution persission if needed.
 # # Parse User File
-# {
-#     read
-#     while IFS=";", read -r email dob group shared
-#     do
-#         # create username from email
-#         # format first letter of first name followed by full last name.
-#         createUserName $email
-#         password=$(date -d $dob +'%m%Y')
-#         echo "Converting $dob to Password: $password"
-#         echo "Converting $email to username:"
+{
+    read
+    while IFS=";", read -r email dob group shared
+    do
+        # create username from email
+        # format first letter of first name followed by full last name.
+        createUserName $email
+        password=$(date -d $dob +'%m%Y')
+        echo "Converting $dob to Password: $password"
+        echo "Converting $email to username:"
         
-#         echo "Groups: $group"
-#         #parse group string
-#         #check if group exists
-#         #create if not
-#         #add this user to group
-#         echo "Shared Folder: $shared"
-#     done
-# } < $FILE
+        echo "Groups: $group"
+        #parse group string
+        #check if group exists
+        #create if not
+        #add this user to group
+        echo "Shared Folder: $shared"
+    done
+} < $FILE
