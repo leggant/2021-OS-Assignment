89fd6ff7daa09217da20be61cfdcbde8cfe4b4cc
Anthony Legg <61127354+leggant@users.noreply.github.com>
2021-05-03 16:11:23 +1200

    Update task1.sh

--- Task-1/task1.sh
+++ Task-1/task1.sh
@@ -231,9 +231,11 @@ checkIfUserExists() {
 
 createUser() {
     echo "Create New User $1";
-    sudo useradd -d /home/$1 -m -s /bin/bash -p $2 $1;
+    sudo useradd -d /home/$1 -m -s /bin/bash $1;
+    # set password
+    sudo passwd $2 $1
     # Force user to chance password
-    # sudo chage -d 0 $1;
+    sudo passwd --expire $1
 }
 
 createSharedFolder() {
