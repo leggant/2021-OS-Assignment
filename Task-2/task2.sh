aaa8fbc017c025f42082e6649f10e64eea558b99
Anthony Legg <>
2021-05-05 23:00:04 +0000

    Update to task 2

--- Task-2/task2.sh
+++ Task-2/task2.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
-
+StartScript() {
 echo -e "\n#####################################\n###### Directory Backup Script ######\n#####################################\n"
-
 echo -e "This Script Will Compress a Directory of Your Choosing and Backup This File To A Remote Server\n"
+read -pr "Do You Wish To Continue? " continue
 
-read -p "Do You Wish To Continue? " continue
-if [[ $continue -eq "yes" ||
-        $continue -eq "y" ||
-        $continue -eq "YES" ||
-    $continue -eq "Y" ]]; then
+if [[ $continue == "yes" ||
+        $continue == "y" ||
+        $continue == "YES" ||
+    $continue == "Y" ]]; then
     echo "$continue ok"
-    elif [[ $continue -eq "no" ||
-        $continue -eq "n" ||
-        $continue -eq "NO" ||
-    $continue -eq "N" ]]; then
+    elif [[ $continue == "no" ||
+        $continue == "n" ||
+        $continue == "NO" ||
+    $continue == "N" ]]; then
     echo "$continue no"
 else
     echo "Sorry I Dont Understand"
 fi
 
-echo $@
-echo $#
+}
+
+StartScript
