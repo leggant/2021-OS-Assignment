64872c9889404defd51478c412db359b5042bc50
Anthony Legg <>
2021-05-20 10:57:02 +0000

    task 2 script started. Also created the BSA text file required for the assignment documentation. Task 2 script has a menu script which tells the user what the script will do. confirms user wishes to continue.

--- Task-2/task2.sh
+++ Task-2/task2.sh
@@ -1,23 +1,37 @@
 #!/bin/bash
+
+
 StartScript() {
 echo -e "\n#####################################\n###### Directory Backup Script ######\n#####################################\n"
 echo -e "This Script Will Compress a Directory of Your Choosing and Backup This File To A Remote Server\n"
 read -p "Do You Wish To Continue? " continue
-
-if [[ $continue == "yes" ||
-        $continue == "y" ||
-        $continue == "YES" ||
-    $continue == "Y" ]]; then
+counter=0
+until [ $counter -eq 3 ]
+do
+if [[ $continue = "yes" ||
+        $continue = "y" ||
+        $continue = "YES" ||
+    $continue = "Y" ]]; then
     echo "$continue ok"
-    elif [[ $continue == "no" ||
-        $continue == "n" ||
-        $continue == "NO" ||
-    $continue == "N" ]]; then
+    return 0
+elif [[ $continue = "no" ||
+        $continue = "n" ||
+        $continue = "NO" ||
+    $continue = "N" ]]; then
     echo "$continue no"
+    exitapp "Now Exiting the Program......"
 else
-    echo "Sorry I Dont Understand"
+    echo "Sorry I Dont Understand, Please Enter Yes Or No"
+    read -p "Do You Wish To Continue? " continue
 fi
+((counter++))
+if [ $counter -eq 3 ]; then
+    echo "$counter attempts failed"
+    echo "Please Try Again Later"
+    exit 1
+fi
+done
+
 
 }
-
 StartScript
