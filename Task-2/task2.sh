3cd3aa392fddab1f25f221d32faf61c7135eacb6
Anthony Legg <>
2021-05-26 11:53:06 +0000

    Starting task 2 - added a function for the user to enter a directory to compress, given a fallback option for the user to just use the current directory.

--- Task-2/task2.sh
+++ Task-2/task2.sh
@@ -1,5 +1,32 @@
 #!/bin/bash
 
+currentPath=$(pwd)
+currentFileName=$(echo "${currentPath##*/}")
+userInputDirectory=""
+exitapp() {
+    clear
+    echo -e "\n\nExiting the Program....."
+    exit 1
+}
+askUserForDirectory() {
+    read -p "Enter the full path to the directory you would like to compress and transfer or press enter if you wish to use the current path: " path
+    if [ ${#path} -eq 0 ]; then
+        userInputDirectory=$currentPath;
+        checkDirectoryExists $userInputDirectory;
+    else 
+        userInputDirectory=$path
+        checkDirectoryExists $userInputDirectory;
+    fi
+}
+checkDirectoryExists() {
+   [[ -d $1 ]] && echo "$1 directory exists!" && userInputDirectory=$1 && return 0;
+   [[ ! -d $1 ]] && echo "$1 directory not exists!" && return 1; 
+}
+getUserInput() {
+    askUserForDirectory
+    echo -e "Current file is $currentFileName\nCurrent Dir is $currentPath"
+    echo $?
+}
 StartScript() {
 echo -e "\n#####################################\n###### Directory Backup Script ######\n#####################################\n"
 echo -e "This Script Will Compress a Directory of Your Choosing and Backup This File To A Remote Server\n"
@@ -8,10 +35,9 @@ counter=1
 until [ $counter -eq 3 ]
 do
 if [[ $continue = "yes" || $continue = "y" || $continue = "YES" || $continue = "Y" ]]; then
-    echo "$continue ok"
+    clear
     return 0
 elif [[ $continue = "no" || $continue = "n" || $continue = "NO" || $continue = "N" ]]; then
-    echo "$continue no"
     exitapp "Now Exiting the Program......"
 else
     echo "Sorry I Dont Understand, Please Enter Yes Or No"
@@ -28,7 +54,7 @@ done
 StartScript
 ok=$?
 if [ $ok -eq 0 ]; then
-    # ask for user inputs
+    getUserInput
 elif [ $ok -eq 1 ]; then
-    #exit program
+    echo "exit program"
 fi
