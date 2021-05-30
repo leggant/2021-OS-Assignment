72457d3e7f372b83ec255e496d2c2f816d625fc0
Anthony Legg <leggtc1@student.op.ac.nz>
2021-05-31 10:43:18 +1200

    Update to task-2 - added some functions that need to be completed
    for the task. Just need to add code to these, plus error checking

--- BSA_Self_Assessment.md
+++ BSA_Self_Assessment.md
@@ -1,45 +1,11 @@
-<h1>Task 1</h1>
+# BSA Self Assessment
 
-
-
-
-
-# Task 1
-
-### Completed
+## Task 1
 
 ## Uresolved Issues
+
 - Unable to provide fallback options for users that do not have a group, or do not have a shared folder provided
 
-## Features
+## Task 2
 
-|  Activity  | Points | Completed | Attempted |
-| :--: | :--: | :--: | :--: |
-| Reading local file / URL |      |  |      |
-|     Self-assessment      |      |      |      |
-|    Lecturer Feedback     |      |  |      |
-
-# Task 2
-
-|  Handling Script Input  |  5  |  Completed  |  []- Attempted    |      |
-| ---- | ---- | ---- | ---- | ---- |
-|      |      |      |      |      |
-|      |      |      |      |      |
-|      |      |      |      |      |
-
-<html>
-
-<h1>Task 1</h1>
-
-<p></p>
-
-</html>
-
-# Formal Aspects 
-
-|  []    |      |      |      |      |
-| ---- | ---- | ---- | ---- | ---- |
-|      |      |      |      |      |
-|      |      |      |      |      |
-| ``   |      |      |      |      |
-
+## Documentation
--- Task-2/task2.sh
+++ Task-2/task2.sh
@@ -5,17 +5,55 @@ currentFileName=$(echo "${currentPath##*/}")
 userInputDirectory=""
 outputFileName="default"
 log="log.txt"
+
+logUser() {
+    message=$1;
+    echo -e "\n>>>> $message";
+    echo -e "\n>>>> $message";
+}
+
+logError() {
+    message=$1;
+    echo -e "\n>>>>ERROR<<<< $message";
+    echo -e "\n>>>>ERROR<<<< $message";
+}
+
 # Destination Ip 
+getDestinationIP() {
+    echo "Get Destination"
+}
+
 # file name
+getZipFileName() {
+    echo "Zip File Name?"
+}
+
 # destination folder
+getDestinationDirectory() {
+    echo "Get Dest Directory"
+}
+
 # port number
+getPortNumber() {
+    echo "Default is 22"
+}
+
 # zip the file
+createZip() {
+    tar -czvf $outputFileName.tar.gz $userInputDirectory 2>>$log;
+}
+
 # send to remote
+sendToRemoteHost() {
+    echo "Send Zip To Remote"
+}
+
 exitapp() {
     clear
     echo -e "\n\nExiting the Program....."
     exit 1
 }
+
 askUserForDirectory() {
     read -p "Enter the full path to the directory you would like to compress and transfer or press enter if you wish to use the current path: " path
     ok=1
@@ -33,7 +71,7 @@ askUserForDirectory() {
     fi
 }
 sendToRemote() {
-#scp -P 22 default.tar.gz #username@ipaddress : inputfiledirectoryonremote
+    scp -P 22 default.tar.gz #username@ipaddress : inputfiledirectoryonremote
 }
 
 checkRemoteOnline() {
@@ -50,28 +88,28 @@ getUserInput() {
     echo $?
 }
 StartScript() {
-echo -e "\n#####################################\n###### Directory Backup Script ######\n#####################################\n"
-echo -e "This Script Will Compress a Directory of Your Choosing and Backup This File To A Remote Server\n"
-read -p "Do You Wish To Continue? " continue
-counter=1
-until [ $counter -eq 3 ]
-do
-if [[ $continue = "yes" || $continue = "y" || $continue = "YES" || $continue = "Y" ]]; then
-    clear
-    return 0
-elif [[ $continue = "no" || $continue = "n" || $continue = "NO" || $continue = "N" ]]; then
-    exitapp "Now Exiting the Program......"
-else
-    echo "Sorry I Dont Understand, Please Enter Yes Or No"
+    echo -e "\n#####################################\n###### Directory Backup Script ######\n#####################################\n"
+    echo -e "This Script Will Compress a Directory of Your Choosing and Backup This File To A Remote Server\n"
     read -p "Do You Wish To Continue? " continue
-fi
-((counter++))
-if [ $counter -eq 3 ]; then
-    echo "$counter attempts failed"
-    echo "Please Try Again Later"
-    exit 1
-fi
-done
+    counter=1
+    until [ $counter -eq 3 ]
+    do
+    if [[ $continue = "yes" || $continue = "y" || $continue = "YES" || $continue = "Y" ]]; then
+        clear
+        return 0
+    elif [[ $continue = "no" || $continue = "n" || $continue = "NO" || $continue = "N" ]]; then
+        exitapp "Now Exiting the Program......"
+    else
+        echo "Sorry I Dont Understand, Please Enter Yes Or No"
+        read -p "Do You Wish To Continue? " continue
+    fi
+    ((counter++))
+    if [ $counter -eq 3 ]; then
+        echo "$counter attempts failed"
+        echo "Please Try Again Later"
+        exit 1
+    fi
+    done
 }
 StartScript
 ok=$?
